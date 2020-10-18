f478cbdd82f0e57000bde3784b050ac118deb6fd
ILNUR ILDAROVICH <73057248+ilturbo@users.noreply.github.com>
2020-10-18 15:02:07 +0300

    Add files via upload

---
+++ Unite_iLTurbo.py
@@ -0,0 +1,10 @@
+import re
+inputList = []
+for file in ['File1.txt','File2.txt']:
+    with open(file,'r') as infile:
+        for line in infile:
+            inputList.extend(re.sub('[^A-Za-z0-9,]+', '', line).split(","))
+print(inputList)
+with open('UniteFile.txt','w') as outfile:
+    for line in inputList:
+        outfile.write(line + '\n')
